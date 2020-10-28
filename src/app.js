e1b7a347222589ea99087b8c5ece1060961c8200
Eduardo Piorini <eduardopiorini@hotmail.com>
2020-10-28 05:10:50 -0300

    create of post

--- src/app.js
+++ src/app.js
@@ -15,7 +15,23 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { 
+    title, 
+    url, 
+    techs
+   } = request.body;
+  
+  const repository = {
+    id: uuid(), 
+    title, 
+    url,
+    techs,
+    likes = 0
+  };
+
+  repositories.push(repository);
+
+  return response.json(project);
 });
 
 app.put("/repositories/:id", (request, response) => {
